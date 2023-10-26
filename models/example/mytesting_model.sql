7c3a89c6cb94a32a5b25bedd36006a06e4ec57c7
Vasileios Anagnostopoulos <fithis2001@gmail.com>
2023-10-26 09:59:00 +0300

    feat: fixes and add seeds

--- models/example/mytesting_model.sql
+++ models/example/mytesting_model.sql
@@ -1,6 +1,6 @@
 
 -- Use the `ref` function to select from other models
-
-select distinct(attributes)
-from {{ ref('title_akas_model') }}
+{{ config(materialized='view') }}
+select distinct("alpha-2")
+from {{ ref('countries_with_regional_codes') }}
 --where id = 1
