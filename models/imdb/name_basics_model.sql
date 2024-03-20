201a063f736677a41fc4a0b8565abed8fd735350
Vasileios Anagnostopoulos <vasilis.anagnostopoulos@agileactors.com>
2024-03-20 15:32:36 +0200

    feat: make tests more robust and remove fake seeds

--- models/imdb/name_basics_model.sql
+++ models/imdb/name_basics_model.sql
@@ -6,8 +6,8 @@ with source_data_name_basics as (
 
     case when nconst='\N' then NULL else nconst end as nconst,
     case when primaryName='\N' then NULL else primaryName end as primaryName,
-    case when birthYear='\N' then NULL else birthYear end as birthYear,
-    case when deathYear='\N' then NULL else deathYear end as deathYear,
+    case when regexp_full_match(birthYear,'[1-9]\d\d\d') then birthYear else NULL end as birthYear,
+    case when regexp_full_match(deathYear,'[1-9]\d\d\d') then deathYear else NULL end as deathYear,
     case when primaryProfession='\N' then NULL else primaryProfession end as primaryProfession,
     case when knownForTitles='\N' then NULL else knownForTitles end as knownForTitles
     
