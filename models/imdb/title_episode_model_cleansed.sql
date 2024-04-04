c2e0cea7d04f330c975dd92da0381ef4ec866d4d
Vasileios Anagnostopoulos <vasilis.anagnostopoulos@agileactors.com>
2024-04-04 21:41:55 +0300

    fix: cleanse tables

--- models/imdb/title_episode_model_cleansed.sql
+++ models/imdb/title_episode_model_cleansed.sql
@@ -1,3 +1,5 @@
 {{ config(materialized='table') }}
 
 select * from {{ref('title_episode_model_casted')}}
+where tconst in (select tconst from {{ref('title_basics_model_cleansed')}})
+and parentTconst in (select tconst from {{ref('title_basics_model_cleansed')}})
--- models/imdb/title_ratings_model_cleansed.sql
+++ models/imdb/title_ratings_model_cleansed.sql
@@ -1,3 +1,4 @@
 {{ config(materialized='table') }}
 
 select * from {{ref('title_ratings_model_casted')}}
+where tconst in (select tconst from {{ref('title_basics_model_cleansed')}})
