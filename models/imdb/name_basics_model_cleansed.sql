372c513c7d18cc4c49c062e046ae711e9e8c515d
Vasileios Anagnostopoulos <vasilis.anagnostopoulos@agileactors.com>
2024-04-05 15:27:50 +0300

    fix: smaller and bigger tests

--- models/imdb/name_basics_model_cleansed.sql
+++ models/imdb/name_basics_model_cleansed.sql
@@ -17,9 +17,13 @@ from {{ref('name_basics_model_casted')}} t,
 LATERAL (select flatten(array_agg(xxx)) from (
     SELECT [knownForTitle] as xxx
     from (select unnest(t.knownForTitles) as knownForTitle)
-    where knownForTitle in (select tconst from title_basics_model)
+    where knownForTitle in (select tconst from {{ref('title_basics_model_cleansed')}})
     UNION ALL 
     SELECT [] as xxx
 )) t2(knownForTitles)
 where primaryName is not NULL
 and birthYear is not NULL
+and birthYear >= 1000 
+and birthYear <= 9999
+and ( (deathYear is null) or ( (deathYear is not null) and (deathYear >= 1000 ) and (deathYear <= 9999)))
+and ( (deathYear is null) or (birthYear is null)  or ( (deathYear is not null) and (birthYear is not null) and (deathYear > birthYear)))
