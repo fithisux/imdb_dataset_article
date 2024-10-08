4fa7130b3a8fcad0c91013953fa7e33f96ae348a
Vasileios Anagnostopoulos <vasilis.anagnostopoulos@agileactors.com>
2024-10-08 18:05:46 +0300

    fix: missing rename of type castings

--- models/imdb_gold/imdb_title_akas_node.sql
+++ models/imdb_gold/imdb_title_akas_node.sql
@@ -3,7 +3,7 @@
 select
 titleId || '_' || cast(ordering as string) as akas_id, 
 titleId,
-cast(ordering as int), 
+cast(ordering as int) as ordering, 
 title, 
 isOriginalTitle 
 from {{ ref('title_akas_model_cleansed') }}
--- models/imdb_gold/imdb_title_principal_node.sql
+++ models/imdb_gold/imdb_title_principal_node.sql
@@ -3,6 +3,6 @@
 select
 tconst || '_' || cast(ordering as string) as principal_id,
 tconst,
-cast(ordering as int), 
+cast(ordering as int) as ordering, 
 job
  from {{ ref('title_principals_model_cleansed') }}
